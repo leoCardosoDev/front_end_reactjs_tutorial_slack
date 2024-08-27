e2cabc16e627938eb4b2d0a800bc106d85a2d0cd
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-27 11:56:49 -0700

    feat: add sign up screen

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -10,8 +10,13 @@ import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+import { SignInFlow } from '../types';
 
-export const SignInCard = () => {
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -68,8 +73,11 @@ export const SignInCard = () => {
         </div>
         <p className="text-xs text-muted-foreground">
           Não tem uma conta?{' '}
-          <span className="text-sky-700 hover:underline cursor-pointer">
-            Sign Up
+          <span
+            onClick={() => setState('signUp')}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Criar conta
           </span>
         </p>
       </CardContent>
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,3 +1,100 @@
-export const SignUpCard = () => {
-  return <div>Sign Up Card</div>;
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { SignInFlow } from '../types';
+
+interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Crie uma conta</CardTitle>
+        <CardDescription>Preencha o formulário para continuar</CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Nome"
+            type="text"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="E-mail"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Senha"
+            type="password"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Confirme a senha"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Cadastrar
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute top-3 left-2.5" /> Criar com
+            Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-3 left-2.5" /> Criar com
+            GitHub
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Já tem conta? Faça o{' '}
+          <span
+            onClick={() => setState('signIn')}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Login
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
 };
