861c26a6475425d2de636a2851874e934a8cd2f3
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-27 14:05:34 -0700

    feat: add and configure convex

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -11,12 +11,17 @@ import { Separator } from '@/components/ui/separator';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { SignInFlow } from '../types';
+import { useState } from 'react';
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -28,32 +33,32 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
         <form className="space-y-2.5">
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             placeholder="Nome"
             type="text"
             required
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="E-mail"
             type="email"
             required
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Senha"
             type="password"
             required
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
             placeholder="Confirme a senha"
             type="password"
             required
