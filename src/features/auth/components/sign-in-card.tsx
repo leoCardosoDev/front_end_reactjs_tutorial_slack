a1ad7859013d296548be45e8dd142ef5b79405ac
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-29 16:23:35 -0700

    feat: add pending login

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -21,9 +21,11 @@ interface SignInCardProps {
 export const SignInCard = ({ setState }: SignInCardProps) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [pending, setPending] = useState(false);
   const { signIn } = useAuthActions();
   const onProviderSignIn = (value: 'github' | 'google') => {
-    signIn(value);
+    setPending(true);
+    signIn(value).finally(() => setPending(false));
   };
 
   return (
@@ -38,7 +40,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
       <CardContent className="space-y-5 px-0 pb-0">
         <form className="space-y-2.5">
           <Input
-            disabled={false}
+            disabled={pending}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="E-mail"
@@ -46,21 +48,21 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Senha"
             type="password"
             required
           />
-          <Button type="submit" className="w-full" size="lg" disabled={false}>
+          <Button type="submit" className="w-full" size="lg" disabled={pending}>
             Entrar
           </Button>
         </form>
         <Separator />
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
+            disabled={pending}
             onClick={() => onProviderSignIn('google')}
             variant="outline"
             size="lg"
@@ -70,7 +72,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             com Google
           </Button>
           <Button
-            disabled={false}
+            disabled={pending}
             onClick={() => onProviderSignIn('github')}
             variant="outline"
             size="lg"
