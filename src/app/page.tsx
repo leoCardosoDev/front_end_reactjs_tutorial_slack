087517fa3a1c311a701d0afce40d54d941fcad3e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-30 07:22:28 -0700

    feat: add user logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-'use client';
 import { UserButton } from '@/features/auth/components/user-button';
-import { useAuthActions } from '@convex-dev/auth/react';
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
       <UserButton />
-      <h1>Home</h1>
     </div>
   );
 }
