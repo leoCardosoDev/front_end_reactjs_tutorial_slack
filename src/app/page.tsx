e87e3915b8ad6ab87c1d0d7baa63e3e4f045997f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-30 07:52:27 -0700

    feat: call workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+'use client';
 import { UserButton } from '@/features/auth/components/user-button';
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
+import { useEffect, useMemo } from 'react';
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+  useEffect(() => {
+    if (isLoading) return;
+    if (workspaceId) {
+      console.log('Redirect to workspace');
+    } else {
+      console.log('Open create modal');
+    }
+  }, [workspaceId, isLoading]);
   return (
     <div>
       <UserButton />
