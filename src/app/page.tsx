c9dcfc7448cae4c47042f2c1e38cab1eac539f98
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-30 06:36:20 -0700

    feat: add button user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@/features/auth/components/user-button';
 import { useAuthActions } from '@convex-dev/auth/react';
 
 export default function Home() {
   const { signOut } = useAuthActions();
   return (
     <div>
+      <UserButton />
       <h1>Home</h1>
-      <Button onClick={() => signOut()}>Sair</Button>
     </div>
   );
 }
