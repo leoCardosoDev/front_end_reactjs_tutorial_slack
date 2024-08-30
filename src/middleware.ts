f53110849d56facfb73c1e0a0b86a1c381e6e004
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-30 06:36:03 -0700

    chore: add avatar e dropdown-menu

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,6 @@ export default convexAuthNextjsMiddleware((request) => {
   if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
     return nextjsMiddlewareRedirect(request, '/auth');
   }
-  if (isProtectedRoute(request) && isAuthenticatedNextjs()) {
-    return nextjsMiddlewareRedirect(request, '/');
-  }
 });
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
