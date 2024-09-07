2a77f18ef6e3174db1bc79544d43266bc67a36b8
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-07 05:58:23 -0700

    feat: add workspace id

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
-  return <div>ID: {workspaceId}</div>;
+  const { data } = useGetWorkspace({ id: workspaceId });
+  return <div>data: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
