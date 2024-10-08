724c35800bdaa6a10c9227b8ec5cfa9fdee4a869
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-08 12:37:13 -0700

    feat: adjust sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -2,12 +2,18 @@ import { UserButton } from '@/features/auth/components/user-button';
 import { WorkspaceSwitcher } from './workspace-switcher';
 import { Bell, Home, MessageSquare, MoreHorizontal } from 'lucide-react';
 import { SidebarButton } from './sidebar-button';
+import { usePathname } from 'next/navigation';
 
 export const Sidebar = () => {
+  const pathName = usePathname();
   return (
     <aside className="w-[70px] h-full bg-[rgb(72,19,73)] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <WorkspaceSwitcher />
-      <SidebarButton icon={Home} label="Home" isActive />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathName.includes('/workspace')}
+      />
       <SidebarButton icon={MessageSquare} label="DMs" />
       <SidebarButton icon={Bell} label="Atividades" />
       <SidebarButton icon={MoreHorizontal} label="Mais" />
